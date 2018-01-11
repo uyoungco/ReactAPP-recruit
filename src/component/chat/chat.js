c43632176c949fdf3ba00446614aa07e752157ed
uyoung <zzyouyang@qq.com>
2018-01-11 21:02:24 +0800

    1.聊天表情的发送

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, InputItem, NavBar, Icon } from 'antd-mobile'
+import { List, InputItem, NavBar, Icon, Grid } from 'antd-mobile'
 import { connect } from 'react-redux';
 import { getMsgList, sendMsg, recvMsg } from '../../redux/chat.reudx'
 import { getChatId } from '../../util'
@@ -13,7 +13,7 @@ class Chat extends React.Component {
         super()
         this.state = {
             text:'',
-            msg:[]
+            showEmoji:false
         }
     }
     componentDidMount() {
@@ -21,6 +21,11 @@ class Chat extends React.Component {
             this.props.getMsgList()
             this.props.recvMsg()
         }
+    }
+    fixCarousel() {
+        setTimeout(function () {
+            window.dispatchEvent(new Event('resize'))
+        }, 0)
     }
     handleSubmit() {
         // socket.emit('sendmsg', {text:this.state.text})
@@ -33,6 +38,10 @@ class Chat extends React.Component {
         this.setState({text:''})
     }
     render() {
+        const emoji = '😀 😃 😄 😁 😆 😅 😂 😊 😇 🙂 🙃 😉 😌 😍 😘 😗 😙 😚 😋 😜 😝 😛 🤑 🤗 🤓 😎 😏 😒 😞 😔 😟 😕 🙁 😣 😖 😫 😩 😤 😠 😡 😶 😐 😑 😯 😦 😧 😮 😲 😵 😳 😱 😨 😰 😢 😥 😭 😓 😪 😴 🙄 🤔 😬 🤐 😷 🤒 🤕 😈 👿 👹 👺 💩 👻 💀 ☠️ 👽 👾 🤖 🎃 😺 😸 😹 😻 😼 😽 🙀 😿 😾 👐 🙌 👏 🙏 👍 👎 👊 ✊ 🤘 👌 👈 👉 👆 👇 ✋  🖐 🖖 👋  💪 🖕 ✍️  💅 🖖 💄 💋 👄 👅 👂 👃 👁 👀 '
+            .split(' ')
+            .filter(v => v)
+            .map(v => ({ text: v }))
         const userid = this.props.match.params.user
         const Item = List.Item
         const users = this.props.chat.users
@@ -78,9 +87,33 @@ class Chat extends React.Component {
                             onChange={v =>{
                                 this.setState({text:v})
                             }}
-                            extra={<span onClick={() => this.handleSubmit()}>发送</span>}
+                            extra={
+                                <div>
+                                    <span style={{marginRight:15}} onClick={() => {
+                                        this.setState({
+                                            showEmoji: !this.state.showEmoji
+                                        })
+                                        this.fixCarousel()
+                                    }}>😃</span>
+                                    <span onClick={() => this.handleSubmit()}>发送</span>
+                                </div>
+                            }
                         ></InputItem>
                     </List>
+                    {this.state.showEmoji ?
+                        <Grid
+                            data={emoji}
+                            columnNum={9}
+                            carouselMaxRow={4}
+                            isCarousel={true}
+                            onClick={el=>{
+                                this.setState({
+                                    text:this.state.text+el.text
+                                })
+                                console.log(el)
+                            }}
+                        /> : null
+                    }
                 </div>
             </div>
         )
